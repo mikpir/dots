f806de9f959d479131d11f3a24e92aa0ca9fcdeb
Mikko Pirhonen <mikkopirho@gmail.com>
2023-09-11 22:49:44 +0300

    Add intel font

--- fonts.sh
+++ fonts.sh
@@ -2,19 +2,18 @@
 
 mkdir -p temp-fonts
 cd temp-fonts
-for FONT_NAME in CascadiaCode JetBrainsMono SourceCodePro
-do
-    SYSDIR=/usr/local/share/fonts/$FONT_NAME
-    sudo mkdir -p $SYSDIR
+for FONT_NAME in CascadiaCode JetBrainsMono SourceCodePro IntelOneMono; do
+  SYSDIR=/usr/local/share/fonts/$FONT_NAME
+  sudo mkdir -p $SYSDIR
 
-    mkdir -p ./$FONT_NAME
-    wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/$FONT_NAME.zip" -O "fonts.zip"
-    unzip fonts.zip -d ./$FONT_NAME
-    sudo mv ./$FONT_NAME/* /usr/local/share/fonts/$FONT_NAME
-   
-    sudo chown -R root: $SYSDIR
-    sudo chmod 644 $SYSDIR/* 
-    sudo restorecon -RF $SYSDIR
+  mkdir -p ./$FONT_NAME
+  wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/$FONT_NAME.zip" -O "fonts.zip"
+  unzip fonts.zip -d ./$FONT_NAME
+  sudo mv ./$FONT_NAME/* /usr/local/share/fonts/$FONT_NAME
+
+  sudo chown -R root: $SYSDIR
+  sudo chmod 644 $SYSDIR/*
+  sudo restorecon -RF $SYSDIR
 done
 
 cd ..
