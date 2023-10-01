a18a72170f514a4a698308ced98d52ff324c8f40
Mikko Pirhonen <mikkopirho@gmail.com>
2023-10-01 14:08:15 +0300

    Clean up unnecessary installs

--- apps.sh
+++ apps.sh
@@ -3,32 +3,20 @@
 # Upgrade everything
 sudo dnf up -y
 
-# Vscode repo
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-sudo dnf check-update
-
-# nvm
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
-
 ### Flatpaks ###
 
-sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-sudo flatpak install -y com.slack.Slack com.bitwarden.desktop com.discordapp.Discord com.spotify.Client io.dbeaver.DBeaverCommunity us.zoom.Zoom
+# sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+# sudo flatpak install -y com.slack.Slack com.bitwarden.desktop com.discordapp.Discord com.spotify.Client io.dbeaver.DBeaverCommunity us.zoom.Zoom
 
 
-sudo dnf install -y speedtest-cli pandoc
+# sudo dnf install -y speedtest-cli pandoc
 
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
 
 # apps
 sudo dnf in -y \
-brightnessctl speedtest-cli hwinfo neofetch \
-grin slurp wl-clipboard fzf z \
-neovim kitty starship \
+brightnessctl hwinfo neofetch \
+fzf \
+neovim kitty \
 python3 \
-yarnpkg \
-code \
-chromedriver google-chrome-stable \
-postgresql
 
