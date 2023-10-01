13f883e61a1235015d257fcadbc5de9978eda0cc
Mikko Pirhonen <mikkopirho@gmail.com>
2023-10-01 14:43:53 +0300

    Fix wget flag

--- apps.sh
+++ apps.sh
@@ -9,7 +9,7 @@ sudo dnf up -y
 # sudo flatpak install -y com.slack.Slack com.bitwarden.desktop com.discordapp.Discord com.spotify.Client io.dbeaver.DBeaverCommunity us.zoom.Zoom
 
 mkdir -p ~/.local/bin
-wget https://raw.githubusercontent.com/rupa/z/master/z.sh -o ~/.local/bin/z.sh
+wget https://raw.githubusercontent.com/rupa/z/master/z.sh -O ~/.local/bin/z.sh
 
 # sudo dnf install -y speedtest-cli pandoc
 
